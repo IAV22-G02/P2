7b66526484d11167bf23e61b197e7624d58156af
Oxelion <juandiem@ucm.es>
2022-03-02 21:34:17 +0100

    Fix de rotacion Euler de  la camara al seguir a Teseo

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,15 +8,17 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public Vector3 cameraOffset;
+    public Vector3 angularOffset;
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 desiredPosition = target.position + cameraOffset;
         Vector3 smoothedPosition = Vector3.Slerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
         transform.LookAt(target);
+        transform.rotation *= Quaternion.Euler(angularOffset);
     }
 }
