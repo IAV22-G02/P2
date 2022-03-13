9a2df6c5482e848cee92c49d8723adc5a0703205
Oxelion <juandiem@ucm.es>
2022-03-13 19:49:49 +0100

    Zoom in/out del laberinto con el mouse

--- Assets/Scripts/Teseo/CameraFollow.cs
+++ Assets/Scripts/Teseo/CameraFollow.cs
@@ -7,10 +7,16 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
 
+    public float scale = 0.5f;
     public float smoothSpeed = 0.125f;
     public Vector3 cameraOffset;
     public Vector3 angularOffset;
 
+    void Update()
+    {
+        cameraOffset.y -= Input.mouseScrollDelta.y * scale;
+        cameraOffset.x += Input.mouseScrollDelta.y * scale * 0.5f;
+    }
 
     // Update is called once per frame
     void LateUpdate() {
